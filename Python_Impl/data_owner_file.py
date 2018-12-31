e64ddbc93cf32983b24514488de628ab5f165584
Enehta <mollerin@eurecom.fr>
2018-12-31 10:37:20 +0100

    added key generation & class structure

--- Python_Impl/data_owner_file.py
+++ Python_Impl/data_owner_file.py
@@ -1,4 +1,11 @@
 import random
 from bplib import bp
+import hashlib
 
-G = bp.BpGroup()
+#global parameters
+tau = 256# security parameter hash functions
+
+class Data_Owner:
+
+    def __init__(self):
+        G = bp.BpGroup()
---
+++ Python_Impl/keyserver_file.py
@@ -0,0 +1,24 @@
+import random
+
+global parameters
+sk_size = 50
+
+
+class Keyserver:
+
+    def __init__(self, generator_g1, generator_g2):
+        """
+        Generates private key and corresponding public key pair
+        :param generator_g1: generator of Group g1
+        :param generator_g2: generator of Group g2
+        """
+        # generate private key
+        self.private_key = random.getrandbits(sk_size)
+        self.public_key_pair = (pow(generator_g1, self.private_key), pow(generator_g2, self.private_key))
+
+    def get_public_key_pair(self):
+        """
+        Returns the public key pair of the Keyserver instance
+        :return: public key pair
+        """
+        return self.public_key_pair
